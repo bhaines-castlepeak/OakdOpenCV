4e99d907febfcb71cd1298dc63e318014286b2f4
knoname <bob@castlepeakinc.com>
2021-02-03 14:59:03 -0700

    change to opencv 4.5.1

--- build.sh
+++ build.sh
@@ -67,13 +67,13 @@ sudo apt -y install \
 
 # start OpenCV build
 cd ~ && \
-    wget -O opencv.zip https://github.com/opencv/opencv/archive/4.5.0.zip && \
-    wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.5.0.zip
+    wget -O opencv.zip https://github.com/opencv/opencv/archive/4.5.1.zip && \
+    wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.5.1.zip
 # unpack
 unzip opencv.zip && \
     unzip opencv_contrib.zip && \
-    mv opencv-4.5.0 opencv && \
-    mv opencv_contrib-4.5.0 opencv_contrib && \
+    mv opencv-4.5.1 opencv && \
+    mv opencv_contrib-4.5.1 opencv_contrib && \
     # clean up the zip files
     rm opencv.zip && \
     rm opencv_contrib.zip 
